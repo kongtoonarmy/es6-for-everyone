5f5928d2096b17d02e47dcf572788c14a1159e1e
Kongyot <kongyot_ro@dtgsiam.com>
2018-01-12 08:10:57 +0700

    46 - Creating your own Modules

--- es6module/app.js
+++ es6module/app.js
@@ -1,7 +1,19 @@
 import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
+import { 
+    apiKey as key, 
+    url, 
+    sayHi, 
+    old, 
+    dog 
+} from './src/config'
+
+console.log(key, url);
+
+sayHi('Kongyot');
 
 const ages = [1,1,4,52,12,4];
 
-console.log(uniq(ages));
+console.log(uniq(ages)); 
+
